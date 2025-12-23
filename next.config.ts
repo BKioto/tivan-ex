0b93bec077cf9479aa2820210bf529f5afb9b193
Bahador <b.eslami97@gmail.com>
2025-12-24 00:44:58 +0330

    Disable strict build checks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* تنظیمات نادیده گرفتن خطاها برای بیلد */
+  eslint: {
+    // هشدار: این گزینه باعث میشه خطاهای ESLint مانع بیلد نشن
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // هشدار: این گزینه باعث میشه خطاهای تایپ‌اسکریپت مانع بیلد نشن
+    ignoreBuildErrors: true,
+  },
+  images: {
+    // برای جلوگیری از گیر دادن به عکس‌های اکسترنال
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
